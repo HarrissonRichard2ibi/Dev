2c61cf98763d45c4b78e93018dcb069cc17c93fd
Harrisson Richard <harrisson.richard@2ibi.com>
2024-07-17 14:32:23 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            File.WriteAllText(Directory.GetCurrentDirectory() + "/DevOps.txt", "Dev Ops Demon \n");
+        }
+    }
+}
